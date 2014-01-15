3759f72c249765c551523edacbb4e4fcb5459d97
Samantha Luber <saluber23@gmail.com>
2014-01-14 16:21:06 -0800

    Placeholder Windows Phone 8 panorama app
    Not that exciting

---
+++ Venmo/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Venmo.Resources;
+
+namespace Venmo
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
